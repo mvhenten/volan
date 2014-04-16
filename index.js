52ba13ebb1d32303506dead38288e09647017982
Matthijs van Henten <matthijs@ischen.nl>
2014-04-16 13:54:53 +0200

    the intiial implementation

---
+++ index.js
@@ -0,0 +1,103 @@
+'use strict';
+
+function format(tpl) {
+    var values = Array.prototype.slice.call(arguments, 1);
+    return tpl.replace(/(%s)/g, values.shift.bind(values));
+}
+
+function isPlainFunction(value) {
+    var own = Object.getOwnPropertyNames((value || {}).prototype || {});
+
+    if (typeof value !== 'function') return false;
+    if (own.length >= 2 || (own.indexOf('constructor') < 0 && own.length >= 1)) return false;
+
+    return Object.getPrototypeOf(value.prototype) === Object.prototype;
+}
+
+function isBuiltIn(value) {
+    return /^(Boolean|Number|String|RegExp|Array|Object|Date)$/.test(value.name);
+}
+
+function isTypeOf(value) {
+    return typeof value === this.name.toLowerCase();
+}
+
+function isInstanceOf(value) {
+    return value instanceof this;
+}
+
+function property(key, def) {
+    var prop = Object.getOwnPropertyDescriptor(def, key),
+        check = prop.value;
+
+    if (!check || !prop.writable) return prop;
+    if (prop.get || prop.set) return prop;
+    if (/^number|string|boolean$/.test(typeof check)) return prop;
+    if (!check.name && isPlainFunction(check)) return prop;
+
+    if (check instanceof RegExp) {
+        check = check.test.bind(check);
+        check.__type = format('value matching "%s"', check);
+    }
+
+    if (check.name === undefined || isBuiltIn(check)) {
+        var o = check;
+        if (/^Boolean|Number|String$/.test(check.name)) check = isTypeOf.bind(o);
+        else check = isInstanceOf.bind(o);
+        check.__type = o.name || o + '';
+    }
+
+    check.__name = key;
+
+    return {
+        get: function() {
+            return this.__meta__(key);
+        },
+
+        set: function(value) {
+            if (check(value)) return this.__meta__(key, value);
+            throw new TypeError(format('Validation failed for "%s", value "%s" is not a %s', check.__name, value, check.__type || check.name));
+        }
+    };
+}
+
+function properties(def) {
+    var proto = {
+        __meta__: {
+            value: null,
+            writable: true
+        }
+    };
+    for (var key in def) proto[key] = property(key, def);
+    return proto;
+}
+
+var Volan = {
+    extend: function(spr, def) {
+        var attributes = Object.keys(def),
+            ctor = function(args) {
+                this.__meta__ = function(key, value) {
+                    if (value !== undefined) this[key] = value;
+                    return this[key];
+                }.bind({});
+
+                if (typeof spr === 'function') spr.apply(this, arguments);
+
+                for (var i = 0; i < attributes.length; i++) {
+                    var key = attributes[i];
+                    if (args && args[key] !== undefined) this[key] = args[key];
+                }
+
+                Object.freeze(this);
+            };
+
+        ctor.prototype = Object.create(spr.prototype || {}, properties(def));
+        return ctor;
+    },
+
+    create: function(def) {
+        return Volan.extend({}, def);
+    }
+};
+
+module.exports = Volan;
---
+++ test/volan.js
@@ -0,0 +1,254 @@
+'use strict';
+
+var assert = require('assert'),
+    _ = require('lodash'),
+    Faker = require('Faker'),
+    Volan = require('../');
+
+var Point = Volan.create({
+    x: Number,
+    y: Number
+});
+
+suite('Volan constructors and tests', function() {
+    test('REGRESSION: getters must not be triggerd in create', function() {
+        var Thing = Volan.create({
+            get cantouchthis() {
+                throw new Error('Can\'t touch this');
+            }
+        });
+
+        assert.throws(function() {
+            var test = new Thing({
+                cantouchthis: 1
+            });
+        }, /TypeError: Cannot set property cantouchthis/)
+    });
+
+
+    test('Example from the readme: point', function() {
+        var Point = Volan.create({
+            x: null,
+            y: null
+        });
+
+        var ax = _.random(0, 99),
+            ay = _.random(0, 99),
+            bx = _.random(0, 99),
+            by = _.random(0, 99);
+
+        var a = new Point({
+            x: ax,
+            y: ay
+        });
+        var b = new Point({
+            x: bx,
+            y: by
+        });
+
+        assert.equal(a.x, ax);
+        assert.equal(a.y, ay);
+
+        assert.equal(b.x, bx);
+        assert.equal(b.y, by);
+    });
+
+
+    test('Instantiate Volan objects', function() {
+        _.times(100, function() {
+            var x = _.random(-999, 999),
+                y = _.random(-999, 999);
+
+            var p = new Point({
+                x: x,
+                y: y
+            });
+
+            assert.equal(p.x, x, 'Constructor has set the expected value');
+            assert.equal(p.y, y, 'Constructor has set the expected value');
+
+            assert.throws(function() {
+                p.x = Faker.Lorem.words();
+            }, /Validation failed .+ not a Number/);
+
+            assert.throws(function() {
+                var p = new Point({
+                    x: Faker.Lorem.words(),
+                    y: y
+                });
+                assert.ok(!p);
+            }, /Validation failed .+ not a Number/);
+        });
+    });
+
+    test('With native method allowed', function() {
+        _.times(100, function() {
+            var words = Faker.Lorem.words().join(' ');
+
+            var Thing = Volan.create({
+                name: words,
+
+                sayHello: function() {
+                    return this.name + ' says "hello"';
+                }
+            });
+
+            var thing = new Thing();
+
+            assert.equal(thing.name, words);
+            assert.equal(thing.sayHello(), thing.name + ' says "hello"');
+        });
+    });
+    test('With native getters allowed', function() {
+        _.times(100, function() {
+            var first = Faker.Name.firstName();
+            var last = Faker.Name.lastName();
+
+            var Person = Volan.create({
+                first: String,
+                last: String,
+
+                get name() {
+                    return [this.last, this.first].join(', ');
+                }
+            });
+
+            var john = new Person({
+                first: first,
+                last: last
+            });
+
+            assert.equal(john.name, [last, first].join(', '));
+
+        });
+    });
+
+    test('With native default value allowed', function() {
+        _.times(100, function() {
+            // words or empty string
+            var words = Faker.Lorem.words().slice(0, _.random(10)).join(' ');
+
+            var Thing = Volan.create({
+                name: words,
+            });
+
+            var thing = new Thing();
+
+            assert.equal(thing.name, words);
+
+        });
+    });
+
+    test('With custom type isa', function() {
+        _.times(100, function() {
+            var value = _.random(-999, 999);
+
+            var Thing = Volan.create({
+                count: function PositiveInt(value) {
+                    return (typeof value == 'number') && value >= 0;
+                },
+            });
+
+            var thing = new Thing();
+
+            thing.count = 42;
+
+            if (value < 0) {
+                assert.throws(function() {
+
+                    thing.count = value;
+                }, /TypeError: Validation failed for "count", value ".+" is not a PositiveInt/);
+            } else {
+                thing.count = value;
+                assert.equal(thing.count, value);
+            }
+        });
+    });
+
+    test('Volan extends plain js', function() {
+        var Parent = function(args) {
+            this._thing = args;
+        };
+
+        Parent.prototype = {
+            get thing() {
+                return this._thing;
+            }
+        };
+
+        var Child = Volan.extend(Parent, {
+            name: String
+        });
+
+        _.times(100, function() {
+            var words = Faker.Lorem.words().join(' '),
+                args = {
+                    name: words
+                };
+
+            var parent = new Parent(args),
+                child = new Child(args);
+
+            assert.deepEqual(parent.thing, args);
+            assert.deepEqual(child.thing, args);
+        });
+    });
+
+    test('Volan extends plain js vanilla', function() {
+        var Parent = function(args) {
+            this._thing = args;
+        };
+
+        Parent.prototype = {
+            get thing() {
+                return this._thing;
+            }
+        };
+
+        var Child = Volan.extend(Parent, {
+            name: String
+        });
+
+
+        _.times(100, function() {
+            var words = Faker.Lorem.words().join(' '),
+                args = {
+                    name: words
+                };
+
+            var parent = new Parent(args),
+                child = new Child(args);
+
+            assert.deepEqual(parent.thing, args);
+            assert.deepEqual(child.thing, args);
+        });
+    });
+
+    test('Volan handles simple declarations', function() {
+        var Any = Volan.create({
+            any: null
+        });
+
+        var a = new Any({
+            any: 42
+        });
+
+        assert.equal(a.any, 42);
+    });
+
+    test('Example code from the readme: extending point', function() {
+        var Point3D = Volan.extend(Point, {
+            z: Number,
+        });
+
+        var point = new Point3D({
+            x: 1,
+            y: 2,
+            z: 4
+        });
+
+        assert.equal(point.x, 1);
+        assert.equal(point.z, 4);
+        assert.equal(point.y, 2);
+    });
+});
